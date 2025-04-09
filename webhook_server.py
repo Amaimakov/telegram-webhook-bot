9b6afa6390b37af15c1ef2228f9db8a9819fb9b8
Amaimakov <acxat-pavlodar@mail.ru>
2025-04-09 10:28:25 +0500

    Update webhook_server.py

--- webhook_server.py
+++ webhook_server.py
@@ -50,5 +50,9 @@ def notify():
 
     return {'status': 'ok'}, 200
 
+import os
+
 if __name__ == '__main__':
-    app.run(port=5050)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
